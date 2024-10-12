a7962e2d0668439deddd4c60b7e0ce0380e8e702
spezifisch <spezifisch+git@gmail.com>
2024-10-12 22:21:39 +0200

    add tests (which fail as of yet) for context inheritance

--- cmd/example1/go.mod
+++ cmd/example1/go.mod
@@ -1,8 +1,9 @@
 module github.com/spezifisch/tview-command/cmd/example1
 
-go 1.22.7
+go 1.22.1
 
 require (
+	github.com/gdamore/tcell/v2 v2.7.1
 	github.com/rivo/tview v0.0.0-20240921122403-a64fc48d7654
 	github.com/spezifisch/tview-command v0.0.0-20240828122932-69552353924a
 )
@@ -10,7 +11,6 @@ require (
 require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
-	github.com/gdamore/tcell/v2 v2.7.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
---
+++ keybinding/context_inheritance_test.go
@@ -0,0 +1,59 @@
+package keybinding_test
+
+import (
+	"testing"
+
+	"github.com/spezifisch/tview-command/keybinding"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContextAddLogic(t *testing.T) {
+	configPath := "../testdata/TestContextAddLogic.toml"
+	config, err := keybinding.LoadConfig(configPath)
+
+	assert.NoError(t, err, "Config should load without error")
+	assert.NotNil(t, config, "Config should not be nil")
+
+	queueContext := (*config)["Queue"]
+	assert.NotNil(t, queueContext, "Queue context should be present")
+
+	// Test keybindings defined directly in Queue
+	assert.Equal(t, "queue.moveTrack", queueContext.Bindings["m"], "Queue-specific binding for key 'm' should be moveTrack")
+
+	// Ensure that bindings from both Default and ListPreset are inherited
+	assert.Equal(t, "goToTop", queueContext.Bindings["g"], "Queue context should inherit key 'g' from ListPreset")
+	assert.Equal(t, "addToQueue", queueContext.Bindings["a"], "Queue context should inherit key 'a' from Default")
+}
+
+func TestContextFallback(t *testing.T) {
+	configPath := "../testdata/TestContextFallback.toml"
+	config, err := keybinding.LoadConfig(configPath)
+
+	assert.NoError(t, err, "Config should load without error")
+	assert.NotNil(t, config, "Config should not be nil")
+
+	queueContext := (*config)["Queue"]
+
+	// Ensure that bindings present in the Queue context are resolved
+	assert.Equal(t, "queue.moveTrack", queueContext.Bindings["m"], "Binding for key 'm' should be defined in Queue")
+
+	// Check fallback to Default context for undefined bindings in Queue
+	defaultContext := (*config)["Default"]
+	assert.Equal(t, "deleteTrack", defaultContext.Bindings["d"], "Default binding for key 'd' should be deleteTrack")
+	assert.Equal(t, defaultContext.Bindings["d"], queueContext.Bindings["d"], "Queue should inherit 'd' from Default")
+}
+
+func TestContextAddInheritance(t *testing.T) {
+	configPath := "../testdata/TestContextAddInheritance.toml"
+	config, err := keybinding.LoadConfig(configPath)
+
+	assert.NoError(t, err, "Config should load without error")
+	assert.NotNil(t, config, "Config should not be nil")
+
+	queueContext := (*config)["Queue"]
+	assert.NotNil(t, queueContext, "Queue context should be present")
+
+	// Ensure keybindings from Default are inherited in Queue
+	assert.Equal(t, "deleteTrack", queueContext.Bindings["d"], "Binding for key 'd' should be inherited from Default")
+	assert.Equal(t, "addToQueue", queueContext.Bindings["a"], "Binding for key 'a' should be inherited from Default")
+}
